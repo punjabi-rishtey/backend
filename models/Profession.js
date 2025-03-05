18b7c7cbe7b0f68ee81af362f6de4b634c227de8
ijcool999 <ishanjain2174@gmail.com>
2025-03-05 20:39:18 +0530

    h

--- models/Profession.js
+++ models/Profession.js
@@ -1,15 +1,35 @@
+// const mongoose = require("mongoose");
+
+// const professionSchema = new mongoose.Schema({
+//   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // ✅ Matches User schema
+//   user_name: { type: String, required: true },
+//   occupation: String,
+//   designation: String,
+//   working_with: String,
+//   income: String,
+//   work_address: String,
+// });
+
+// const Profession = mongoose.model("Profession", professionSchema);
+// module.exports = Profession;
+
+
 const mongoose = require("mongoose");
 
 const professionSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // ✅ Matches User schema
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   user_name: { type: String, required: true },
   occupation: String,
   designation: String,
   working_with: String,
-  working_as: String,
   income: String,
-  work_address: String,
+  work_address: {
+    address: { type: String, default: "" },
+    city: { type: String, default: "" },
+  },
 });
+
+
 
 const Profession = mongoose.model("Profession", professionSchema);
 module.exports = Profession;
