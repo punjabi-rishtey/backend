25a2b56924a9a049ace21a57981c5ec8ac48c3b0
ijcool999 <ishanjain2174@gmail.com>
2025-04-02 21:37:55 +0530

    Testimonials apis added

--- models/Testimonial.js
+++ models/Testimonial.js
@@ -5,7 +5,12 @@ const testimonialSchema = new mongoose.Schema({
   message: { type: String, required: true },
   image_url: { type: String }, // Cloudinary image URL
   image_public_id: { type: String }, // Cloudinary public ID for deletion
-  created_at: { type: Date, default: Date.now }
+  created_at: { type: Date, default: Date.now },
+
+  groom_registration_date: { type: Date },
+  bride_registration_date: { type: Date },
+  marriage_date: { type: Date }
+
 });
 
 const Testimonial = mongoose.model("Testimonial", testimonialSchema);
